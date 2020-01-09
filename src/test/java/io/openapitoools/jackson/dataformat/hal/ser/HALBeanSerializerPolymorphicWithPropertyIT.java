437b7682c0e73b4adfd3d2b31f1a639b3cc8f5a5
Philipp Westphalen <philipp.westphalen@sda.se>
2020-01-09 10:49:06 +0100

    Add test cases for polymorphic classes without List and other Annotation (as.property)

--- src/test/java/io/openapitoools/jackson/dataformat/hal/ser/HALBeanSerializerPolymorphicWithPropertyIT.java
+++ src/test/java/io/openapitoools/jackson/dataformat/hal/ser/HALBeanSerializerPolymorphicWithPropertyIT.java
@@ -6,6 +6,7 @@ import com.fasterxml.jackson.annotation.JsonSubTypes;
 import com.fasterxml.jackson.annotation.JsonTypeInfo;
 import com.fasterxml.jackson.annotation.JsonTypeInfo.As;
 import com.fasterxml.jackson.annotation.JsonTypeInfo.Id;
+import com.fasterxml.jackson.annotation.JsonTypeName;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import io.openapitools.jackson.dataformat.hal.HALLink;
 import io.openapitools.jackson.dataformat.hal.HALMapper;
@@ -18,12 +19,30 @@ import java.util.Collection;
 import java.util.List;
 import org.junit.Test;
 
-public class HALBeanSerializerPolymorphicIT {
+public class HALBeanSerializerPolymorphicWithPropertyIT {
 
   ObjectMapper om = new HALMapper();
 
   @Test
   public void testSerializationForResourceWithEmbeddableList() throws Exception {
+    @Resource
+    class TopResource {
+
+      public String id = "1";
+
+      @Link public HALLink self = new HALLink.Builder(URI.create("/top/1")).build();
+
+      @Link("child")
+      public List<HALLink> childResourcesLink =
+          Arrays.asList(
+              new HALLink.Builder(URI.create("/top/1/child/1")).build(),
+              new HALLink.Builder(URI.create("/top/1/child/2")).build());
+
+      @EmbeddedResource("child")
+      public Collection<ChildResource> children =
+          Arrays.asList(new ChildResource("1"), new OtherChildResource("2", "Max"));
+    }
+
     TopResource resource = new TopResource();
     String json = om.writeValueAsString(resource);
     assertEquals(
@@ -34,8 +53,8 @@ public class HALBeanSerializerPolymorphicIT {
             + "},"
             + "\"_embedded\":{"
             + "\"child\":["
-            + "{\"_links\":{\"self\":{\"href\":\"/top/1/child/1\"}},\"type\":\"ChildResource\",\"id\":\"1\"},"
-            + "{\"_links\":{\"self\":{\"href\":\"/top/1/child/2\"}},\"type\":\"OtherChildResource\",\"id\":\"2\",\"name\":\"Max\"}]"
+            + "{\"_links\":{\"self\":{\"href\":\"/top/1/child/1\"}},\"@type\":\"ChildResource\",\"id\":\"1\"},"
+            + "{\"_links\":{\"self\":{\"href\":\"/top/1/child/2\"}},\"@type\":\"OtherChildResource\",\"id\":\"2\",\"name\":\"Max\"}]"
             + "},"
             + "\"id\":\"1\"}",
         json);
@@ -43,6 +62,16 @@ public class HALBeanSerializerPolymorphicIT {
 
   @Test
   public void testSerializationForResourceWithList() throws Exception {
+    @Resource
+    class TopResourceWithoutEmbedded {
+      public String id = "1";
+
+      @Link public HALLink self = new HALLink.Builder(URI.create("/top/1")).build();
+
+      public Collection<ChildResource> children =
+          Arrays.asList(new ChildResource("1"), new OtherChildResource("2", "Max"));
+    }
+
     TopResourceWithoutEmbedded resource = new TopResourceWithoutEmbedded();
     String json = om.writeValueAsString(resource);
     assertEquals(
@@ -52,9 +81,59 @@ public class HALBeanSerializerPolymorphicIT {
             + "},"
             + "\"id\":\"1\","
             + "\"children\":["
-            + "{\"_links\":{\"self\":{\"href\":\"/top/1/child/1\"}},\"type\":\"ChildResource\",\"id\":\"1\"},"
-            + "{\"_links\":{\"self\":{\"href\":\"/top/1/child/2\"}},\"type\":\"OtherChildResource\",\"id\":\"2\",\"name\":\"Max\"}]"
+            + "{\"_links\":{\"self\":{\"href\":\"/top/1/child/1\"}},\"@type\":\"ChildResource\",\"id\":\"1\"},"
+            + "{\"_links\":{\"self\":{\"href\":\"/top/1/child/2\"}},\"@type\":\"OtherChildResource\",\"id\":\"2\",\"name\":\"Max\"}]"
             + "}",
+        json);
+  }
+
+  @Test
+  public void testSerializationForResourceEmbedded() throws Exception {
+    @Resource
+    class SimpleTopResourceEmbedded {
+      public String id = "1";
+
+      @Link public HALLink self = new HALLink.Builder(URI.create("/top/1")).build();
+
+      @EmbeddedResource
+      public ChildResource child = new ChildResource("1");
+    }
+
+    SimpleTopResourceEmbedded resource = new SimpleTopResourceEmbedded();
+    String json = om.writeValueAsString(resource);
+    assertEquals("{"
+        + "\"_links\":{"
+        + "\"self\":{\"href\":\"/top/1\"}"
+        + "},"
+        + "\"_embedded\":{"
+        + "{\"_links\":{\"self\":{\"href\":\"/top/1/child/1\"}},\"@type\":\"ChildResource\",\"id\":\"1\"},"
+        + "},"
+        + "\"id\":\"1\"}",
+        json);
+  }
+
+  @Test
+  public void testSerializationForResource() throws Exception {
+    @Resource
+    class SimpleTopResource {
+      public String id = "1";
+
+      @Link public HALLink self = new HALLink.Builder(URI.create("/top/1")).build();
+
+      public ChildResource child = new ChildResource("1");
+    }
+
+    SimpleTopResource resource = new SimpleTopResource();
+    String json = om.writeValueAsString(resource);
+    assertEquals(
+        "{"
+            + "\"_links\":{"
+            + "\"self\":{\"href\":\"/top/1\"}"
+            + "},"
+            + "\"id\":\"1\","
+            + "\"child\":{"
+            + "\"_links\":{\"self\":{\"href\":\"/top/1/child/1\"}},\"@type\":\"ChildResource\",\"id\":\"1\""
+            + "}}",
         json);
   }
 
@@ -67,7 +146,7 @@ public class HALBeanSerializerPolymorphicIT {
             + "\"_links\":{"
             + "\"self\":{\"href\":\"/top/1/child/1\"}"
             + "},"
-            + "\"type\":\"ChildResource\","
+            + "\"@type\":\"ChildResource\","
             + "\"id\":\"1\"}",
         json);
   }
@@ -81,51 +160,20 @@ public class HALBeanSerializerPolymorphicIT {
             + "\"_links\":{"
             + "\"self\":{\"href\":\"/top/1/child/1\"}"
             + "},"
-            + "\"type\":\"ChildResource\","
+            + "\"@type\":\"ChildResource\","
             + "\"id\":\"1\","
             + "\"name\";\"Max\"}",
         json);
   }
 
-  @Resource
-  public static class TopResource {
-
-    public String id = "1";
-
-    @Link public HALLink self = new HALLink.Builder(URI.create("/top/1")).build();
-
-    @Link("child")
-    public List<HALLink> childResourcesLink =
-        Arrays.asList(
-            new HALLink.Builder(URI.create("/top/1/child/1")).build(),
-            new HALLink.Builder(URI.create("/top/1/child/2")).build());
-
-    @EmbeddedResource("child")
-    public Collection<ChildResource> children =
-        Arrays.asList(new ChildResource("1"), new OtherChildResource("2", "Max"));
-  }
-
-  @Resource
-  public static class TopResourceWithoutEmbedded {
-    public String id = "1";
-
-    @Link public HALLink self = new HALLink.Builder(URI.create("/top/1")).build();
-
-    public Collection<ChildResource> children =
-        Arrays.asList(new ChildResource("1"), new OtherChildResource("2", "Max"));
-  }
-
   @JsonTypeInfo(
       use = Id.NAME,
-      include = As.EXISTING_PROPERTY,
-      property = "type",
+      include = As.PROPERTY,
       defaultImpl = ChildResource.class)
   @JsonSubTypes({@JsonSubTypes.Type(value = OtherChildResource.class, name = "OtherChildResource")})
+  @JsonTypeName("ChildResource")
   @Resource
   public static class ChildResource {
-
-    public String type = "ChildResource";
-
     public String id;
 
     @Link public HALLink self;
@@ -138,8 +186,6 @@ public class HALBeanSerializerPolymorphicIT {
 
   @Resource
   public static class OtherChildResource extends ChildResource {
-
-    public String type = "OtherChildResource";
 
     public String name;
 
